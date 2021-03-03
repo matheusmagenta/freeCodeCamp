e6c4ddcfd9cc39697390af1a2a848b428693b5e8
matheusmagenta <matheus.magenta@gmail.com>
2021-03-03 14:26:05 +0000

    adding mobile menu animated

--- landingPage/app/js/script.js
+++ landingPage/app/js/script.js
@@ -1,19 +1,27 @@
 const btnHamburguer = document.querySelector("#btnHamburger");
 const header = document.querySelector(".header");
+const body = document.querySelector("body");
 const overlay = document.querySelector(".overlay");
+const fadeElems = document.querySelectorAll(".has-fade");
 
 btnHamburguer.addEventListener("click", function () {
   console.log("click hamburger");
 
   if (header.classList.contains("open")) {
+    body.classList.remove("noscroll");
     // close hamburger
     header.classList.remove("open");
-    overlay.classList.remove("fade-in");
-    overlay.classList.add("fade-out");
+    fadeElems.forEach(function (element) {
+      element.classList.remove("fade-in");
+      element.classList.add("fade-out");
+    });
   } else {
     //open hamburger
+    body.classList.add("noscroll");
     header.classList.add("open");
-    overlay.classList.add("fade-in");
-    overlay.classList.remove("fade-out");
+    fadeElems.forEach(function (element) {
+      element.classList.add("fade-in");
+      element.classList.remove("fade-out");
+    });
   }
 });
